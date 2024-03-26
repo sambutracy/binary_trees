4b43ff6650ab0be717e2317c26a0d4008739a9c6
sambutracy <tracysambut@gmail.com>
2024-03-26 06:32:07 -0700

    task 2: insert right

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - Inserts a node as the right-child of another node
+ * @parent: Pointer to the node to insert the right-child in
+ * @value: Value to store in the new node
+ *
+ * Return: Pointer to the created node, or NULL on failure or if parent is NULL
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	/* Check if parent is NULL */
+	if (parent == NULL)
+		return (NULL);
+
+	/* Allocate memory for the new node */
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+		return (NULL);
+
+	/* If parent already has a right child */
+	if (parent->right != NULL)
+	{
+		/* Set the new node's right child to parent's current right child */
+		new_node->right = parent->right;
+		/* Set the parent's current right child's parent to the new node */
+		parent->right->parent = new_node;
+	}
+
+	/* Set the parent's right child to the new node */
+	parent->right = new_node;
+
+	return (new_node);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -19,4 +19,6 @@ void binary_tree_print(const binary_tree_t *tree);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 /*Task 1*/
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+/*Task 2*/
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 #endif /* BINARY_TREES_H */
