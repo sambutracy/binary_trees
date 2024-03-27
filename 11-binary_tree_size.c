19760c0adfb79014dab3995441282314ddf9bb3c
vintage254 <derricknjuguna414@gmail.com>
2024-03-27 04:42:01 -0700

    betty fix

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -2,14 +2,14 @@
 
 /**
  * binary_tree_size -caclulates the size of a binary tree
- * @tree: root node 
+ * @tree: root node
  *
  * Return: zero if node is empty ,size of node
  **/
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (tree == NULL)
-		return 0;
+		return (0);
 	else
-		return(binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
+		return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
 }
